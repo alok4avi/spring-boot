4d083c2ad84b5872b5284121ccee0bdc876e98b5
Alok <alok4avi@gmail.com>
2020-07-30 18:46:38 -0500

    Update ConsumeMultiPageResponseController.java
    Adding method to parse json with springframework api(no third party api)

--- consume-api-pagination/src/main/java/com/learn/rest/pagination/ConsumeMultiPageResponseController.java
+++ consume-api-pagination/src/main/java/com/learn/rest/pagination/ConsumeMultiPageResponseController.java
@@ -1,6 +1,10 @@
 package com.learn.rest.pagination;
 
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +26,7 @@ public class ConsumeMultiPageResponseController {
 		int total_pages = 0;
 		total_pages = result.getTotal_pages();
 		// home team
-		for (int page=2; page <= total_pages; page++ ) {
+		for (int page = 2; page <= total_pages; page++) {
 			result = template.getForObject("https://jsonmock.hackerrank.com/api/football_matches?year=" + year
 					+ "&team1=" + teamName + "&page=" + page, SampleResponse.class);
 			List<MatchData> matchData = result.getData();
@@ -30,10 +34,10 @@ public class ConsumeMultiPageResponseController {
 				totalGoals += data.getTeam1goals();
 			}
 		}
-		//away team
+		// away team
 		result = template.getForObject(awayUrl, SampleResponse.class);
 		total_pages = result.getTotal_pages();
-		for (int page=2; page <= total_pages; page++ ) {
+		for (int page = 2; page <= total_pages; page++) {
 			result = template.getForObject("https://jsonmock.hackerrank.com/api/football_matches?year=" + year
 					+ "&team2=" + teamName + "&page=" + page, SampleResponse.class);
 			List<MatchData> matchData = result.getData();
@@ -45,4 +49,32 @@ public class ConsumeMultiPageResponseController {
 
 	}
 
+	@GetMapping(value = "/page/json")
+	public Integer getGoals(@RequestParam(value = "year", required = true) Integer year,
+			@RequestParam(value = "team", required = true) String teamName,
+			@RequestParam(value = "page", required = true) String pageNum) {
+		String homeUrl = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year + "&team1=" + teamName
+				+ "&page=" + pageNum;
+		String awayUrl = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year + "&team2=" + teamName
+				+ "&page=" + pageNum;
+		RestTemplate template = new RestTemplate();
+		String response = template.getForObject(homeUrl, String.class);
+		JsonParser springParser = JsonParserFactory.getJsonParser();
+		Map<String, Object> map = springParser.parseMap(response);
+		int total_pages = (int) map.get("total_pages");
+		List<Object> list = (List<Object>) map.get("data");
+		int goal = 0;
+		for (Object o : list) {
+			if (o instanceof Map) {
+				Map<String, Object> datamap = (Map<String, Object>) o;
+				for (Map.Entry<String, Object> entry : datamap.entrySet()) {
+					if (entry.getKey() == "team1goals") {
+						goal += Integer.valueOf(entry.getValue().toString());
+					}
+				}
+			}
+
+		}
+		return goal;
+	}
 }
