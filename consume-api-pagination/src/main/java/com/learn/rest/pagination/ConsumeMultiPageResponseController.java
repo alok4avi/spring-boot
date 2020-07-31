41ac3b004aa380f31265e51ee633949b66db5d39
Alok <alok4avi@gmail.com>
2020-07-31 14:47:48 -0500

    Update ConsumeMultiPageResponseController.java

--- consume-api-pagination/src/main/java/com/learn/rest/pagination/ConsumeMultiPageResponseController.java
+++ consume-api-pagination/src/main/java/com/learn/rest/pagination/ConsumeMultiPageResponseController.java
@@ -1,8 +1,20 @@
 package com.learn.rest.pagination;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +38,7 @@ public class ConsumeMultiPageResponseController {
 		int total_pages = 0;
 		total_pages = result.getTotal_pages();
 		// home team
-		for (int page = 2; page <= total_pages; page++) {
+		for (int page = 1; page <= total_pages; page++) {
 			result = template.getForObject("https://jsonmock.hackerrank.com/api/football_matches?year=" + year
 					+ "&team1=" + teamName + "&page=" + page, SampleResponse.class);
 			List<MatchData> matchData = result.getData();
@@ -37,7 +49,7 @@ public class ConsumeMultiPageResponseController {
 		// away team
 		result = template.getForObject(awayUrl, SampleResponse.class);
 		total_pages = result.getTotal_pages();
-		for (int page = 2; page <= total_pages; page++) {
+		for (int page = 1; page <= total_pages; page++) {
 			result = template.getForObject("https://jsonmock.hackerrank.com/api/football_matches?year=" + year
 					+ "&team2=" + teamName + "&page=" + page, SampleResponse.class);
 			List<MatchData> matchData = result.getData();
@@ -74,6 +86,85 @@ public class ConsumeMultiPageResponseController {
 				}
 			}
 
+		}
+		return goal;
+	}
+
+	@GetMapping(value = "/page/parse")
+	public Integer getTotalGoal(@RequestParam(value = "year", required = true) Integer year,
+			@RequestParam(value = "team", required = true) String teamName) {
+		int totalGoals = 0;
+		try {
+			String homeUrl = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year + "&team1="
+					+ URLEncoder.encode(teamName, StandardCharsets.UTF_8.toString()) + "&page=";
+			String awayUrl = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year + "&team2="
+					+ URLEncoder.encode(teamName, StandardCharsets.UTF_8.toString()) + "&page=";
+			totalGoals = aggregateTotalGoals(homeUrl) + aggregateTotalGoals(awayUrl);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return totalGoals;
+	}
+	
+	
+	/**
+	 * Method to invoke REST service based upon url
+	 * @param baseUrl
+	 * @param year
+	 * @param teamName
+	 * @return
+	 */
+	private int aggregateTotalGoals(String baseUrl) {
+		int goal = 0;
+		int page = 1;
+		Object obj;
+		JSONObject jsonMap;
+		BufferedReader br;
+		URL url;
+		HttpURLConnection conn;
+		String response;
+		try {
+			url = new URL(baseUrl + page);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+			br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			response = br.readLine();
+			System.out.println("Output from Server .... \n");
+			if (response != null) {
+				obj = new JSONParser().parse(response);
+				jsonMap = (JSONObject) obj;
+				Long totalPages = (Long) jsonMap.get("total_pages");
+				while (page <= totalPages.intValue()) {
+					System.out.println(response);
+					obj = new JSONParser().parse(response);
+					jsonMap = (JSONObject) obj;
+					List<Object> list = (List<Object>) jsonMap.get("data");
+					for (Object o : list) {
+						if (o instanceof Map) {
+							Map<String, Object> datamap = (Map<String, Object>) o;
+							goal += Integer.valueOf(datamap.get("team1goals").toString());
+						}
+					}
+					++page;
+					url = new URL(baseUrl + page);
+					conn = (HttpURLConnection) url.openConnection();
+					br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+					response = br.readLine();
+				}
+			}
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
 		return goal;
 	}
